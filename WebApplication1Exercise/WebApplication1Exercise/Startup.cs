8387624eb8484c5159b1fefbfc0934cda4ebf791
agungpradnya <gbagung.pradnyaandikaputra@mitrais.com>
2015-08-18 18:33:31 +0800

    COMMIT INITIATE FILES

---
+++ WebApplication1Exercise/WebApplication1Exercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1Exercise.Startup))]
+namespace WebApplication1Exercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
